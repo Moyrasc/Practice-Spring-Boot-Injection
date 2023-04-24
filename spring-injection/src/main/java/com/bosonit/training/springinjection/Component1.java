c57ce81e9f2d84ef91d47244aaf9bd2159be98c8
Moyrasc <noemisolac@gmail.com>
2023-04-24 13:44:11 +0200

    create component one

---
+++ spring-injection/src/main/java/com/bosonit/training/springinjection/Component1.java
@@ -0,0 +1,13 @@
+package com.bosonit.training.springinjection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Component1 {
+    public Component1(){
+        System.out.println("Hi from component 1");
+    }
+    public String hello(){
+        return "Hello!!";
+    }
+}
