6c785c2ed23fa4a4f2df03539a260ba0e6a619c9
Moyrasc <noemisolac@gmail.com>
2023-04-24 13:32:02 +0200

    practice spring injection dependencies

---
+++ spring-injection/src/main/java/com/bosonit/training/springinjection/SpringInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.training.springinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInjectionApplication.class, args);
+	}
+
+}
